da9fbbcfec894ea22f6d31ddc78e1c4fbc597cd4
andrejhicks <andrejhicks@gmail.com>
2021-11-08 22:30:31 -0500

    UpdateTest

--- app.py
+++ app.py
@@ -1,14 +1,48 @@
-import os
-
+# -*- coding: utf-8 -*-
+import json
+import base64
+import datetime
+import requests
+import pathlib
+import math
+import pandas as pd
+import flask
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import chart_studio.plotly as py
+import plotly.graph_objs as go
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+from dash.dependencies import Input, Output, State
+from plotly import tools
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app = dash.Dash(
+    __name__, meta_tags=[{"name": "viewport", "content": "width=device-width"}],
+)
+
+app.title = "FOREX Web Trader"
 
 server = app.server
+
+PATH = pathlib.Path(__file__).parent
+DATA_PATH = PATH.joinpath("data").resolve()
+
+# Loading historical tick data
+currency_pair_data = {
+    "EURUSD": pd.read_csv(
+        DATA_PATH.joinpath("EURUSD.csv.gz"), index_col=1, parse_dates=["Date"]
+    ),
+    "USDJPY": pd.read_csv(
+        DATA_PATH.joinpath("USDJPY.csv.gz"), index_col=1, parse_dates=["Date"]
+    ),
+    "GBPUSD": pd.read_csv(
+        DATA_PATH.joinpath("GBPUSD.csv.gz"), index_col=1, parse_dates=["Date"]
+    ),
+    "USDCHF": pd.read_csv(
+        DATA_PATH.joinpath("USDCHF.csv.gz"), index_col=1, parse_dates=["Date"]
+    ),
+}
 
 app.layout = html.Div([
     html.H2('Hello World'),
